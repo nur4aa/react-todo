70860c9a5694c90ac85419c039aa0265c1a0b2bc
nur4aa <38027647+nur4aa@users.noreply.github.com>
2021-03-26 14:46:12 +0600

    init

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import TodoList from "./Todo/TodoList";
+import Context from "./context";
 
 function App() {
+  const [
+    todos,
+    setTodos,
+  ] /*деструктуризация массива, (1)-state(измен)  */ = React.useState([
+    { id: 1, completed: false, title: "buy Bread" },
+    { id: 2, completed: false, title: "buy Cola" },
+    { id: 3, completed: false, title: "buy Milk" },
+  ]); //задача начального состояния | всегда возвращает 2 элементов | 1(СОСТОЯНИЕ) 2(ФУНКЦИЯ) ПОЗВОЛЯЮЩАЯ ИЗМЕНЯТЬ ДАННОЕ СОСТОЯНИЕ ДЛЯ ТОГО ЧТОБЫ РЕКАКТ ВИДЕЛ ЭТИ ИЗМЕНЕНИЯ
+
+  function toggleTodo(id) {
+    setTodos(
+      todos.map((todo) => {
+        if (todo.id === id) {
+          todo.completed = !todo.completed;
+        }
+        return todo;
+      })
+    );
+  }
+
+  function removeTodo(id) {
+    setTodos(todos.filter((todo) => todo.id !== id));
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Context.Provider value={{ removeTodo }}>
+      <div className="wrapper">
+        <h1>React todo-List</h1>
+        <TodoList todos={todos} onToggle={toggleTodo}></TodoList>
+      </div>
+    </Context.Provider>
   );
 }
 
